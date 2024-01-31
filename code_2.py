4eebfd2015a1dcf73bf87d454320a3e4c55fa05b
voganhaov <152450101+voganhaov@users.noreply.github.com>
2024-01-31 21:06:20 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+
+
+import random
+        print(f"Random Number: {item}")
+    main()
+if __name__ == "__main__":
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+def main():
+def generate_random_data():
+
+    return data
