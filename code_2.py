216bf0104e525d365939c381c328e19ac2581085
voganhaov <152450101+voganhaov@users.noreply.github.com>
2024-02-01 05:04:12 +0000

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,14 @@
-
-
-import random
-        print(f"Random Number: {item}")
-    main()
-if __name__ == "__main__":
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
     for item in data:
 
-def main():
-def generate_random_data():
+        print(f"Random Number: {item}")
+import random
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
 
-    return data
+def main():
+
+
+    data = generate_random_data()
+    main()
