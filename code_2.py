939d2f4a1b324168ddb3efbe14167abd894fa9b9
voganhaov <152450101+voganhaov@users.noreply.github.com>
2024-02-02 19:04:22 +0000

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,12 +1,29 @@
-        }
+    return data;
+    });
+function generateRandomData() {
+    }
+}
+        console.log("Random Number: " + item);
+
+
+}
+function main() {
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+    const data = [];
+main();        }
+        for (int item : data) {
 public class RandomDataGenerator {
             System.out.println("Random Number: " + item);
     public static void main(String[] args) {
-        for (int item : data) {
-
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-}
         for (int i = 0; i < 10; i++) {
-    }
         int[] data = new int[10];
+        }
+
+    }
+}
+
+            data[i] = (int) (Math.random() * 100) + 1;
